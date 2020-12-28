2b051ac737a8d131bd9b6f6b846d881c106b403a
Jiacheng <110479617@qq.com>
2020-12-28 20:34:23 +0800

    delete testbranch file

--- src/testadd.java
+++ src/testadd.java
@@ -2,6 +2,6 @@ package src;
 
 public class testadd {
     public static void main(String[] args) {
-
+        System.out.println("test add a java file");
     }
 }
--- src/testbranch.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public class testbranch {
-    public static void main(String[] args) {
-        System.out.println("We are testing a new branch");
-    }
-}
